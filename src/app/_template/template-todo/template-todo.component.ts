05cc086ae9d6b5568d1bf16137f09ade21658524
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-09-01 12:31:01 +0200

    add svg assets icons

--- src/app/_template/template-todo/template-todo.component.ts
+++ src/app/_template/template-todo/template-todo.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-template-todo',
   templateUrl: './template-todo.component.html',
-  styleUrl: './template-todo.component.scss'
+  styleUrl: './template-todo.component.sass'
 })
 export class TemplateTodoComponent {
 
