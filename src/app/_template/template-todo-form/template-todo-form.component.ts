9ee7011040eefdb50405827bf5d9c9bf89d08d3d
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-09-01 23:24:39 +0200

    declare createToDo funct with templates

--- src/app/_template/template-todo-form/template-todo-form.component.ts
+++ src/app/_template/template-todo-form/template-todo-form.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../_interface/todo';
 
 @Component({
   selector: 'app-template-todo-form',
   templateUrl: './template-todo-form.component.html',
   styleUrl: './template-todo-form.component.sass'
 })
-export class TemplateTodoFormComponent {
+export class TemplateTodoFormComponent implements OnInit {
 
+
+  public ToDo:Todo
+
+  constructor() {
+    this.ToDo = {
+      id: undefined,
+      label: undefined,
+      status: false,
+      position: undefined
+    }
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  public createToDo(event?: any): void {
+    
+    console.log(this.ToDo)
+    this.ToDo = {
+      id: undefined,
+      label: undefined,
+      status: false,
+      position: undefined
+    }
+  }
 }
