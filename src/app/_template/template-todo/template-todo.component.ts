147b06d7870f8cf9629baabe3dba5066ee034934
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-09-01 22:54:15 +0200

    add prog logic to todo components

--- src/app/_template/template-todo/template-todo.component.ts
+++ src/app/_template/template-todo/template-todo.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../_interface/todo';
 
 @Component({
   selector: 'app-template-todo',
   templateUrl: './template-todo.component.html',
   styleUrl: './template-todo.component.sass'
 })
-export class TemplateTodoComponent {
+export class TemplateTodoComponent implements OnInit {
 
+    public ToDo:Todo
+
+    constructor() {
+      this.ToDo = {
+        id: 1,
+        label: 'Wie geht',
+        status: false,
+        position: 1
+      }
+    }
+
+    ngOnInit(): void {
+      
+    }
+
+    public changeCheck(event?: any): void {
+      this.ToDo.status = !this.ToDo.status
+    }
 }
