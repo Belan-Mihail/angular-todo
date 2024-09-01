74e33f44447ff27ba9674b212d85fa8d3327c252
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-09-01 08:15:17 +0200

    add header styles

--- src/app/_template/template-header/template-header.component.ts
+++ src/app/_template/template-header/template-header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-template-header',
   templateUrl: './template-header.component.html',
-  styleUrl: './template-header.component.scss'
+  styleUrl: './template-header.component.sass'
 })
 export class TemplateHeaderComponent {
 
